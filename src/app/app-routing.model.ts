cc146fdef6a966a157445b287c16b1956a98de0b
David Silva <david_goncalvessilva@hotmail.com>
2023-12-14 13:09:21 +0000

    Add home and error page to Navigation

--- src/app/app-routing.model.ts
+++ src/app/app-routing.model.ts
@@ -1,16 +1,21 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
 import { ExperienceComponent } from "./experience/experience.component";
 import { FormationComponent } from "./formation/formation.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { EducationComponent } from "./education/education.component";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes = [
+    { path: '', component: HomeComponent },
     { path: 'experience', component: ExperienceComponent },
     { path: 'education', component: EducationComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'formations', component: FormationComponent },
+    { path: 'not-found', component: ErrorPageComponent, data: { message: 'Page not found!' } },
+    { path: '**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
@@ -18,4 +23,4 @@ const appRoutes = [
     exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FormationComponent } from './formation/formation.component';
 import { EducationComponent } from './education/education.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { AppRoutingModule } from './app-routing.model';
+import { HomeComponent } from './home/home.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { AppRoutingModule } from './app-routing.model';
     ExperienceComponent,
     FormationComponent,
     EducationComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
